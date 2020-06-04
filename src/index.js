912599b6c6f037eed6f8611abba34dbb3446153e
Marcelo Escobar <escobar@br.systems>
2020-06-04 19:46:08 -0300

    include functions and change routes

--- src/index.js
+++ src/index.js
@@ -1,48 +1,23 @@
 var express = require('express');
-var fs = require('fs');
-
 var app = express();
 
+var fs = require('fs');
+
+var accountsRouter = require('./routes/accounts.js');
+global.fileName = 'accounts.json';
+
 app.use(express.json());
-
-app.post('/account', (req, res) => {
-  let account = req.body;
-
-  fs.readFile('accounts.json', 'utf-8', (err, data) => {
-    if (!err) {
-      try {
-        let json = JSON.parse(data);
-        account = { id: json.nextId, ...account};
-
-        json.nextId++;
-        json.accounts.push(account);
-
-        fs.writeFile('accounts.json', JSON.stringify(json), err => {
-          if (err) {
-            res.status(400).send({ error: err.message });
-          } else {
-            res.send({"new_account": account});
-          }
-        });
-
-      } catch (err) {
-          res.status(400).send({ error: err.message });
-      }
-    } else {
-      res.status(400).send({ error: 'Error reading file' });
-    }
-  });
-});
+app.use('/account', accountsRouter);
 
 app.listen(3000, function() {
   try {
-    fs.readFile('accounts.json', 'utf-8', (err, data) => {
+    fs.readFile(global.fileName, 'utf-8', (err, data) => {
       if (err) {
         const initialJson = {
           nextId: 1,
           accounts: []
         };
-        fs.writeFile('accounts.json', JSON.stringify(initialJson), err => {
+        fs.writeFile(global.fileName, JSON.stringify(initialJson), err => {
           if (err) {
             console.log(err);
           }
